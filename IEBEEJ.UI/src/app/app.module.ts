358f2ed9094094bc1141d463c7f37775224e6e31
Cedric Tistaert <cedric.tistaert@gmail.com>
2024-05-22 11:55:38 +0200

    components aangemaakt met angular

--- IEBEEJ.UI/src/app/app.module.ts
+++ IEBEEJ.UI/src/app/app.module.ts
@@ -3,10 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { WebShopComponent } from './web-shop/web-shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AccountComponent } from './account/account.component';
+import { ContactComponent } from './contact/contact.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
+import { Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomePageComponent,
+    WebShopComponent,
+    ProductDetailsComponent,
+    AccountComponent,
+    ContactComponent,
+    ShoppingCartComponent,
+    FooterComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +39,14 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'web-shop', component: WebShopComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'log-in', component: LogInComponent },
+  { path: 'register', component: RegisterComponent }
+];
---
+++ IEBEEJ.UI/src/app/web-shop/web-shop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-shop',
+  templateUrl: './web-shop.component.html',
+  styleUrl: './web-shop.component.css'
+})
+export class WebShopComponent {
+
+}
