fcc2b4b7c85441c0f0b6c061d4e3d103c3412cf1
Cedric Tistaert <cedric.tistaert@gmail.com>
2024-06-03 14:43:11 +0200

    Formatting

--- IEBEEJ/DTOs/UserDTOs/AddUserDTO.cs
+++ IEBEEJ/DTOs/UserDTOs/AddUserDTO.cs
@@ -6,13 +6,15 @@ namespace IEBEEJ.DTOs.UserDTOs
 {
     public class AddUserDTO
     {
-        //public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
         public string Adress { get; set; }
 
-        public string PhoneNumber { get; set; }
         public DateTime Birthday { get; set; }
+
+        public string Email { get; set; }
+
+        //public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
